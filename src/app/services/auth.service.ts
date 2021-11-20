4975319cfcffa90668fd7d73ed39bd299056aab9
Rolando HR <rolando167@hotmail.com>
2021-11-20 11:17:48 -0500

    info y sevicios

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  // 127.0.0.1:8000/api/usuarios/register
+  // 127.0.0.1:8000/api/usuarios/login
+
+  constructor() { }
+}
