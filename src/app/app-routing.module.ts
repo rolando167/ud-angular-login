fe8c45f4b9dcc544eae6f854f904c7f71863ee0d
Rolando HR <rolando167@hotmail.com>
2021-11-20 15:20:44 -0500

    login rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'home'    , component: HomeComponent },
+  { path: 'home'    , component: HomeComponent, canActivate:[ AuthGuard ] },
   { path: 'registro', component: RegistroComponent },
   { path: 'login'   , component: LoginComponent },
-  { path: '**', redirectTo: 'registro' }
+  { path: '**', redirectTo: 'login' }
 ];
 
 @NgModule({
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -78,4 +78,9 @@ export class AuthService {
     return this.userToken;
   }
 
+  estaAutenticado(): boolean{
+
+    return this.userToken.length > 2;
+  }
+
 }
